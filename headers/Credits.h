0ba598700232d82af30fcb9bb580f83b7dcb3b89
Максимилиан <max4tim@bk.ru>
2023-10-18 15:52:56 +0300

    Credits renderFrame template

--- headers/Credits.h
+++ headers/Credits.h
@@ -20,20 +20,29 @@
   */
 class Credits {
 public:
-    Credits();                                                  ///< Create empty object (calling Credits::upload is necessary)
-    
+    Credits();                                               ///< Create empty object (calling Credits::upload is necessary)
+
     ///< Upload images with credits text and layout (text : RGBA, layout : RGB)
     Credits(const std::string& fileNameText, const std::string& fileNameLayout);
 
     ///< Upload images with credits text and layout (text : RGBA, layout : RGB)
     void upload(const std::string& fileNameText, const std::string& fileNameLayout);
 
-    void setAppearVelocity(const cv::Vec2d& appearVelocity);    ///< Set velocity of time shift (pixels per frame)
-    void setDisappearDelay(const double& disappearDelay);       ///< Time from appearing to disappearing (in frames)
+    void setAppearVelocity(const cv::Vec2d& appearVelocity); ///< Set velocity of time shift (pixels per frame)
+    void setDisappearDelay(const double& disappearDelay);    ///< Time from appearing to disappearing (in frames)
 
-    void render(const std::string& outputPath);
+    void render(const std::string& outputPath);              ///< Render credits to the output path
 
 private:
+    ///< Render one frame to the output path & reuturn false if frame is empty
+    bool renderFrame(const std::string& outputPath, const int& time);
+    ///< Render one credits line to the "frame" & edit imgLayout erasing rendered part & reuturn false if (the rendered part of the) frame is empty
+    bool renderLine(cv::Mat& frame, cv::Mat& imgLayout, const cv::Point& rootPixel, const int& time);
+
+
+
+    static bool isPixelObjectMarker(const cv::Mat& imgLayout, const cv::Point& pixel); ///< Check if pixel is object marker
+
     cv::Mat imgLayout;
     cv::Mat imgText;
 
--- sources/Credits.cpp
+++ sources/Credits.cpp
@@ -2,6 +2,8 @@
 
 #include "Credits.h"
 
+const int OBJECT_MARKER_R = 255;
+
 using namespace std;
 using namespace cv;
 
@@ -28,5 +30,36 @@ void Credits::setDisappearDelay(const double& disappearDelay) {
 }
 
 void Credits::render(const std::string& outputPath) {
-    imwrite(outputPath + "Test.png", imgText);
+    // imwrite(outputPath + "Test.png", imgText);
+
+    for (int time = 0; renderFrame(outputPath, time); ++time);
+
+}
+
+bool Credits::renderFrame(const std::string& outputPath, const int& time) {
+    /// Rendering destroys imgLayout & imgText
+    Mat imgLayout;
+    Mat frame(this->imgText.size(), this->imgText.type());
+    this->imgLayout.copyTo(imgLayout);
+
+    bool isFrameNotEmpty = false;
+
+    for (Point pixel(0, 0); pixel.x < imgLayout.cols; ++pixel.x) {
+        for (pixel.y = 0; pixel.y < imgLayout.rows; ++pixel.y) {
+            if (isPixelObjectMarker(imgLayout, pixel)) {
+                isFrameNotEmpty |= renderLine(frame, imgLayout, pixel, time);
+            }
+        }
+    }
+
+    return isFrameNotEmpty;
+}
+
+bool Credits::renderLine(Mat& frame, Mat& imgLayout, const Point& rootPixel, const int& time) {
+
+}
+
+bool Credits::isPixelObjectMarker(const cv::Mat& imgLayout, const Point& pixel) {
+    if (imgLayout.at<Vec3b>(pixel)[2] == OBJECT_MARKER_R) return true;
+    else return false;
 }
